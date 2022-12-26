b162e761cca1d35830dce76d49638fa9a3da99fa
Vasyl <dvorak1981@yandex.ru>
2022-12-26 03:13:47 +0300

    Done all tasks for home work 6.

--- app/src/main/java/com/example/cinema/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/cinema/viewmodel/MainViewModel.kt
@@ -1,28 +1,60 @@
 package com.example.cinema.viewmodel
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.cinema.model.MovieLoader
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.cinema.R
+import com.example.cinema.model.DetailsService
+import com.example.cinema.model.REQUEST_MOVIE
 import com.example.cinema.model.Repository
 import com.example.cinema.model.RepositoryImpl
 import com.example.cinema.model.gson_kinopoisk_API.MovieDTO
+import com.example.cinema.view.Extensions
+
+
+const val DETAILS_INTENT_FILTER = "DETAILS INTENT FILTER"
+
+const val DETAILS_LOAD_RESULT_EXTRA = "LOAD RESULT"
+const val DETAILS_INTENT_EMPTY_EXTRA = "INTENT IS EMPTY"
+const val DETAILS_DATA_EMPTY_EXTRA = "DATA IS EMPTY"
+const val DETAILS_RESPONSE_EMPTY_EXTRA = "RESPONSE IS EMPTY"
+const val DETAILS_REQUEST_ERROR_EXTRA = "REQUEST ERROR"
+const val DETAILS_REQUEST_ERROR_MESSAGE_EXTRA = "REQUEST ERROR MESSAGE"
+const val DETAILS_URL_MALFORMED_EXTRA = "URL MALFORMED"
+const val DETAILS_RESPONSE_SUCCESS_EXTRA = "RESPONSE SUCCESS"
+const val DETAILS_CONDITION_EXTRA = "NEW REQUEST"
+const val PROCESS_ERROR = "Обработка ошибки"
 
 class MainViewModel(
     val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
     private val repositoryImpl: Repository = RepositoryImpl(),
 ) : ViewModel() {
+
+    private lateinit var context_VM: Context
+    private lateinit var mainView_VM: View
+
     var liveDataCurrent = MutableLiveData<AppState>()
     fun getAboutMovie() = getDataFromLocalSource(true)
     fun getUpcomingMovie() = repositoryImpl.getAboutMovieLocalStorageUpcoming()
 
     private fun getDataFromLocalSource(isNowPlaying: Boolean) {
         liveDataToObserve.value = AppState.Loading
+        context_VM.let {
+            LocalBroadcastManager.getInstance(it).unregisterReceiver(loadResultsReceiver)
+        }
         Thread {
 
             liveDataToObserve.postValue(
                 AppState.Success(
                     if (isNowPlaying) {
+
                         repositoryImpl.getAboutMovieLocalStorageNowPlaying()
 
                     } else {
@@ -33,33 +65,73 @@ class MainViewModel(
         }.start()
     }
 
-    fun getDataFromRemoteSource(find_request: String?, context: Context?) {
+    fun getDataFromRemoteSource(
+        find_request: String?,
+        context: Context?,
+        mainView: ConstraintLayout
+    ) {
+        mainView_VM = mainView
+        context_VM = context!!
 
-        var loader = MovieLoader(
-            onLoadListener, find_request, context
-        )
-        loader.loadMovie()
-        try {
+        LocalBroadcastManager.getInstance(context)
+            .registerReceiver(loadResultsReceiver, IntentFilter(DETAILS_INTENT_FILTER))
+        context.startService(Intent(context, DetailsService::class.java).apply {
+            putExtra(REQUEST_MOVIE, find_request)
+        })
 
-        } catch (e: NullPointerException) {
-        }
+
     }
 
     fun getData(): MutableLiveData<AppState> {
         return liveDataToObserve
     }
 
-    private val onLoadListener: MovieLoader.MovieLoaderListener =
-        object : MovieLoader.MovieLoaderListener {
 
-            override fun onLoaded(movieDTO: MovieDTO) {
-                repositoryImpl.getAboutMovieFromServer(movieDTO)
-                getDataFromLocalSource(true)
-            }
+    private val loadResultsReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            Log.d("TAG", "onReceive")
 
-            override fun onFailed(throwable: Throwable) {
+            val strExtra = intent.getStringExtra(DETAILS_LOAD_RESULT_EXTRA)
+
+            when (strExtra) {
+                DETAILS_INTENT_EMPTY_EXTRA -> Extensions.showToast(
+                    mainView_VM,
+                    context_VM.resources.getString(R.string.error_reading)
+                )
+                DETAILS_DATA_EMPTY_EXTRA -> Extensions.showToast(
+                    mainView_VM,
+                    context_VM.resources.getString(R.string.error_reading)
+                )
+                DETAILS_RESPONSE_EMPTY_EXTRA -> Extensions.showToast(
+                    mainView_VM,
+                    context_VM.resources.getString(R.string.error_reading)
+                )
+                DETAILS_REQUEST_ERROR_EXTRA -> Extensions.showToast(
+                    mainView_VM,
+                    context_VM.resources.getString(R.string.error_reading)
+                )
+                DETAILS_REQUEST_ERROR_MESSAGE_EXTRA -> Extensions.showToast(
+                    mainView_VM,
+                    context_VM.resources.getString(R.string.error_reading)
+                )
+                DETAILS_URL_MALFORMED_EXTRA -> Extensions.showToast(
+                    mainView_VM,
+                    context_VM.resources.getString(R.string.error_reading)
+                )
+
+                DETAILS_RESPONSE_SUCCESS_EXTRA -> {
+                    var movieDTO_from_broadcast =
+                        intent.getParcelableExtra<MovieDTO>(DETAILS_CONDITION_EXTRA)
+                    movieDTO_from_broadcast?.let {
+                        repositoryImpl.getAboutMovieFromServer(movieDTO_from_broadcast)
+                        getDataFromLocalSource(true)
+                    }
+
+
+                }
             }
         }
+    }
 
 
 }
