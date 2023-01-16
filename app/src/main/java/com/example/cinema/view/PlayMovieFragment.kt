9c61308b5c6d7418c7af39400be4a5713a036755
Vasyl <dvorak1981@yandex.ru>
2023-01-16 07:38:01 +0300

    Play movie fragment was corrected.

--- app/src/main/java/com/example/cinema/view/PlayMovieFragment.kt
+++ app/src/main/java/com/example/cinema/view/PlayMovieFragment.kt
@@ -1,31 +1,21 @@
 package com.example.cinema.view
 
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.MediaController
+import android.webkit.WebSettings
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import com.example.cinema.R
 import com.example.cinema.databinding.FragmentPlayMovieBinding
-import com.example.cinema.model.model_stuio.Docs
 import com.example.cinema.viewmodel.DetailsFragmentViewModel
-import com.example.cinema.viewmodel.PlayViewModel
+import kotlinx.android.synthetic.main.fragment_play_movie.*
+
 
 class PlayMovieFragment : Fragment() {
     private var _binding: FragmentPlayMovieBinding? = null
     private val binding get() = _binding!!
-
-
-    private val viewModelPlay: PlayViewModel by lazy {
-        ViewModelProvider(this).get(PlayViewModel::class.java)
-    }
 
 
     companion object {
@@ -50,68 +40,19 @@ class PlayMovieFragment : Fragment() {
         _binding = null
     }
 
-    // private lateinit var aboutMovieBundle: Docs
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //   aboutMovieBundle = arguments?.getParcelable(BUNDLE_MOVIE) ?: Docs()
-        binding.webview.visibility = View.GONE
-        binding.videoView.visibility = View.GONE
-        binding.idTVHeading.visibility = View.GONE
-
         val model =
             ViewModelProviders.of(requireActivity()).get(DetailsFragmentViewModel::class.java)
-        model.getSelected().observe(viewLifecycleOwner, { item ->
-            val movie_point =
-                (item.url_trailer[item.url_trailer.length - 4]).toString()
-            if (movie_point == ".") {
-                displayMovie(item)
-            } else {
-                displayWebPage(item)
+        model.getSelected().observe(viewLifecycleOwner) { item ->
+            val webView: WebView = binding.webview
+            val settings: WebSettings = webview.settings
+            webView.loadUrl(item.url_trailer)
+            settings.apply {
+                javaScriptEnabled = true
+                setSupportZoom(true)
+                builtInZoomControls = true
             }
-        })
-
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun displayWebPage(docs_data: Docs) {
-        binding.webview.visibility = View.VISIBLE
-        viewModelPlay.fromDetailFragment(docs_data)
-
-        val observer = Observer<String> {
-            binding.webview.loadDataWithBaseURL(
-                null, it, "text/html; charset=utf-8", "utf-8", null
-            )
         }
-        viewModelPlay.getPlayData().observe(viewLifecycleOwner, observer)
-
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun displayMovie(docs_data: Docs) {
-        binding.videoView.visibility = View.VISIBLE
-        binding.idTVHeading.visibility = View.VISIBLE
-        try {
-            with(binding) {
-                idTVHeading.text = docs_data.name
-                val videoUrl = docs_data.url_trailer
-                val uri: Uri = Uri.parse(videoUrl)
-                videoView.setVideoURI(uri)
-                val mediaController = MediaController(requireContext())
-                mediaController.setAnchorView(videoView)
-                mediaController.setMediaPlayer(videoView)
-                videoView.setMediaController(mediaController)
-                videoView.start()
-
-            }
-        } catch (e: IndexOutOfBoundsException) {
-            Extensions.showSnackbar(binding.videoView,
-                requireContext().resources.getString(R.string.Movie_could_not_find),
-                requireContext().resources.getString(R.string.OK),
-                { requireContext().resources.getString(R.string.OK) })
-            fragmentManager?.popBackStack()
-        }
-
     }
 }
